4a79b6d48c25be9a28cb793d5e954e0cf0e1057f
Damian Cur <damian.cur91@gmail.com>
2021-03-13 18:07:09 +0100

    added "addElement" feature

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const btn = document.querySelector("button");
 let number = 1;
 
 const addElement = function () {
-    
+
     const div = document.createElement("div");
     document.body.appendChild(div);
     div.textContent = number;
+
+    if (number % 5 === 0) {
+        div.classList.add("circle")
+    }
+
+
+
+
+
     number++;
 }
 
-btn.addEventListener("click" , addElement);
-
+btn.addEventListener("click", addElement);
